3020589e282386b2d215271a6236f35557a99a6e
WillBest06 <williambest133@gmail.com>
2025-01-27 12:11:13 +0000

    add text file filter function

--- nameReplacement.py
+++ nameReplacement.py
@@ -1,12 +1,31 @@
 import pathlib
 
 def getListOfFilesFromFolder():
-    folderPath = input('Please enter the path of the folder you wish to open:')
+    folderPath = input('Please enter the path of the folder you wish to open: ')
     textFileFolder = pathlib.Path(folderPath)
 
     listOfFiles = list(textFileFolder.iterdir())
     
     return listOfFiles
 
-l = getListOfFilesFromFolder()
-print(l)
+
+
+def filterForTextFiles(listOfFiles):
+    listOfTextFiles = []
+
+    for file in listOfFiles:
+        if file.suffix == '.txt':
+            listOfTextFiles.append(file)
+        else:
+            continue
+
+    return listOfTextFiles
+
+def main():
+    l = getListOfFilesFromFolder()
+
+    numberOfTextFiles = len(filterForTextFiles(l))
+    print(numberOfTextFiles)
+
+main()
+    
