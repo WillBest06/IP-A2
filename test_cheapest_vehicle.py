374e3eee70415c5d09d24efa9a084e52476a7d2d
WillBest06 <williambest133@gmail.com>
2025-01-28 13:58:58 +0000

    add more tests

--- test_cheapest_vehicle.py
+++ test_cheapest_vehicle.py
@@ -1,6 +1,6 @@
 import cheapestVehicle, unittest
 
-class testCheapestVehicle(unittest.TestCase): 
+class testCheapestVehicle(unittest.TestCase):
     def testCalcVanCost(self):
         self.assertEqual(cheapestVehicle.calcVanCost(0), 37)
         self.assertEqual(cheapestVehicle.calcVanCost(100), 47)
@@ -16,8 +16,24 @@ class testCheapestVehicle(unittest.TestCase):
         self.assertEqual(cheapestVehicle.calcBargeCost(100), 51.63)
         self.assertEqual(cheapestVehicle.calcBargeCost(100.44), 51.65)
 
-    def testGetCheapestVehicle(self):
-        self.assertEqual(cheapestVehicle.getCheapestVehicle(), )
-        self.assertEqual(cheapestVehicle.getCheapestVehicle(), )
-        self.assertEqual(cheapestVehicle.getCheapestVehicle(), )
+    def testGetCheapestVehicle(self):  
+        listOfVehicles = [
+            {'name': 'Van', 'cost': 100},
+            {'name': 'Lorry', 'cost': 200},
+            {'name': 'Barge', 'cost': 220}
+        ]
+        self.assertEqual(cheapestVehicle.getCheapestVehicle(listOfVehicles), {'name': 'Van', 'cost': 100})
 
+        listOfVehicles = [
+            {'name': 'Van', 'cost': 200},
+            {'name': 'Lorry', 'cost': 190},
+            {'name': 'Barge', 'cost': 220}
+        ]
+        self.assertEqual(cheapestVehicle.getCheapestVehicle(listOfVehicles), {'name': 'Lorry', 'cost': 190})
+        
+        listOfVehicles = [
+            {'name': 'Van', 'cost': 200},
+            {'name': 'Lorry', 'cost': 220},
+            {'name': 'Barge', 'cost': 190}
+        ]
+        self.assertEqual(cheapestVehicle.getCheapestVehicle(listOfVehicles), {'name': 'Barge', 'cost': 190})
