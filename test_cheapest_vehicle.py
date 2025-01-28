21dcca2ed685bbda25b23af71b2e351d41030007
WillBest06 <williambest133@gmail.com>
2025-01-28 12:29:52 +0000

    update unit tests

--- test_cheapest_vehicle.py
+++ test_cheapest_vehicle.py
@@ -4,3 +4,20 @@ class testCheapestVehicle(unittest.TestCase):
     def testCalcVanCost(self):
         self.assertEqual(cheapestVehicle.calcVanCost(0), 37)
         self.assertEqual(cheapestVehicle.calcVanCost(100), 47)
+        self.assertEqual(cheapestVehicle.calcVanCost(100.44), 47.04)
+
+    def testCalcLorryCost(self):
+        self.assertEqual(cheapestVehicle.calcLorryCost(0), 47)
+        self.assertEqual(cheapestVehicle.calcLorryCost(100), 52.33)
+        self.assertEqual(cheapestVehicle.calcLorryCost(100.44), 52.35)
+
+    def testBargeLorryCost(self):
+        self.assertEqual(cheapestVehicle.calcBargeCost(0), 47)
+        self.assertEqual(cheapestVehicle.calcBargeCost(100), 51.63)
+        self.assertEqual(cheapestVehicle.calcBargeCost(100.44), 51.65)
+
+    def testGetCheapestVehicle(self):
+        self.assertEqual(cheapestVehicle.getCheapestVehicle(), )
+        self.assertEqual(cheapestVehicle.getCheapestVehicle(), )
+        self.assertEqual(cheapestVehicle.getCheapestVehicle(), )
+
